bfdd63270f5bd620cb3455a573eaa497673b9607
nikita <nikitafedorovv@gmail.com>
2022-01-25 08:20:16 +0300

    Add some placemarks

--- src/main/java/com/smartour/app/views/createroute/CreateRouteView.java
+++ src/main/java/com/smartour/app/views/createroute/CreateRouteView.java
@@ -14,6 +14,9 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
+import java.util.Arrays;
+import java.util.List;
+
 @PageTitle("Create Route")
 @Route(value = "tour", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
@@ -23,6 +26,17 @@ public class CreateRouteView extends VerticalLayout {
     private GoogleMap gmaps;
     private TextField searchQueryField;
     private Button searchButton;
+
+    private List<LatLon> points = Arrays.asList(
+            new LatLon(59.942349742927796, 30.281962732436185),
+            new LatLon(59.950989629918396, 30.299901346327786),
+            new LatLon(59.95455668758736, 30.334834436537747),
+            new LatLon(59.94572664333772, 30.371486976520313),
+            new LatLon(59.94301503285979, 30.352775886432422),
+            new LatLon(59.9344160677453, 30.369856193439258),
+            new LatLon(59.91979270910294, 30.384619071857227),
+            new LatLon(59.91092953816051, 30.34281952656914)
+    );
 
     public CreateRouteView() {
         setSizeFull();
@@ -58,17 +72,16 @@ public class CreateRouteView extends VerticalLayout {
                                         + "; Longitude: "
                                         + ev.getLongitude()));
 
-        // create marker on map center and add info window to it
-        GoogleMapMarker flowingmarker =
-                gmaps.addMarker(
-                        "Center",
-                        gmaps.getCenter(),
-                        false,
-                        null);
-        flowingmarker.addInfoWindow("""
-                <h1>Sightseeing name</h1>
-                                
-                <text>Sightseeing description</text>""");
+        points.forEach(point -> {
+            String name = "Sightseeing " + Double.toString(point.getLat()).substring(0, 5);
+            GoogleMapMarker marker = gmaps.addMarker(
+                    name,
+                    point,
+                    false,
+                    null
+            );
+            marker.addInfoWindow("<h1>" + name + "</h1>" + "<text>Sightseeing description</text>");
+        });
 
         HorizontalLayout searchLayout = new HorizontalLayout(searchQueryField, searchButton);
         searchLayout.setVerticalComponentAlignment(Alignment.BASELINE, searchQueryField, searchButton);
