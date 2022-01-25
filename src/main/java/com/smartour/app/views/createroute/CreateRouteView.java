0db7e5a159f63b20c575eb871772438dfc943df2
nikita <nikitafedorovv@gmail.com>
2022-01-25 07:53:33 +0300

    Initial commit

--- src/main/java/com/smartour/app/views/createroute/CreateRouteView.java
+++ src/main/java/com/smartour/app/views/createroute/CreateRouteView.java
@@ -1,9 +1,13 @@
 package com.smartour.app.views.createroute;
 
+import com.flowingcode.vaadin.addons.googlemaps.GoogleMap;
+import com.flowingcode.vaadin.addons.googlemaps.GoogleMapMarker;
+import com.flowingcode.vaadin.addons.googlemaps.LatLon;
 import com.smartour.app.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -14,22 +18,62 @@ import com.vaadin.flow.server.auth.AnonymousAllowed;
 @Route(value = "tour", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
 @AnonymousAllowed
-public class CreateRouteView extends HorizontalLayout {
+public class CreateRouteView extends VerticalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private GoogleMap gmaps;
+    private TextField searchQueryField;
+    private Button searchButton;
 
     public CreateRouteView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+        setSizeFull();
+
+        searchQueryField = new TextField("Route topic");
+        searchButton = new Button("Create");
+        searchButton.addClickListener(e -> {
+            Notification.show("Loading route \"" + searchQueryField.getValue() + "\"...");
         });
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        String apiKey = "AIzaSyCEQdmi84t3titEn8xHkf7pCZ2ll5Otcv4";
 
-        add(name, sayHello);
+        gmaps = new GoogleMap(apiKey, null, null);
+        gmaps.setMapType(GoogleMap.MapType.ROADMAP);
+        gmaps.setSizeFull();
+        gmaps.setCenter(new LatLon(59.9311, 30.3609));
+
+        // add click listener to get latitude and longitude on left click
+        gmaps.addClickListener(
+                ev ->
+                        Notification.show(
+                                "Left click at latitude: "
+                                        + ev.getLatitude()
+                                        + "; Longitude: "
+                                        + ev.getLongitude()));
+
+        // add click listener to get latitude and longitude on right click
+        gmaps.addRightClickListener(
+                ev ->
+                        Notification.show(
+                                "Right click at latitude: "
+                                        + ev.getLatitude()
+                                        + "; Longitude: "
+                                        + ev.getLongitude()));
+
+        // create marker on map center and add info window to it
+        GoogleMapMarker flowingmarker =
+                gmaps.addMarker(
+                        "Center",
+                        gmaps.getCenter(),
+                        false,
+                        null);
+        flowingmarker.addInfoWindow("""
+                <h1>Sightseeing name</h1>
+                                
+                <text>Sightseeing description</text>""");
+
+        HorizontalLayout searchLayout = new HorizontalLayout(searchQueryField, searchButton);
+        searchLayout.setVerticalComponentAlignment(Alignment.BASELINE, searchQueryField, searchButton);
+
+        add(searchLayout, gmaps);
     }
 
 }
